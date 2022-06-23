4229b97220186ea88a9b3e5f4a3c1a0bff005897
Igor Axenov <aksfasr@gmail.com>
2022-06-23 20:55:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moviesapi/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moviesapi.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviesapi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
